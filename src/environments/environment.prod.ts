f3b7ae67f32dd4fa51fcdc0680e45b42e0e94bc2
Albert-21 <alberto.ah14@hotmail.com>
2021-11-25 10:31:01 -0600

    Se edito la configuracion de las credenciales para el modo de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDGBpURSTFhUw6E5wwmFXSUg2QUqWVySk0",
-    authDomain: "la-montana-f5982.firebaseapp.com",
-    databaseURL: "https://la-montana-f5982-default-rtdb.firebaseio.com",
-    projectId: "la-montana-f5982",
-    storageBucket: "la-montana-f5982.appspot.com",
-    messagingSenderId: "921190507635",
-    appId: "1:921190507635:web:386ee9dfefedd5d288b606"
+    apiKey: "AIzaSyAhpO6vqnY5vpAK4l4k5WUOp4BsBtHFcuc",
+    authDomain: "la-montania.firebaseapp.com",
+    projectId: "la-montania",
+    storageBucket: "la-montania.appspot.com",
+    messagingSenderId: "640250500674",
+    appId: "1:640250500674:web:f2751e62578fea24867fdb"
   }
 };
